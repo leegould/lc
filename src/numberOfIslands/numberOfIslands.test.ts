52ca6af5436db7c837dbbddb528a720ffa12f28e
Lee <gould.lee@googlemail.com>
2020-06-25 12:05:32 +0100

    number of islands setup

---
+++ src/numberOfIslands/numberOfIslands.test.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import { numIslands } from './numberOfIslands';
+
+describe('numberOfIslands', () => {
+    it('runs as expected', () => {
+        const input = [
+            ['1','1','1','1','0'],
+            ['1','1','0','1','0'],
+            ['1','1','0','0','0'],
+            ['0','0','0','0','0'],
+        ];
+
+        const expected = 1;
+
+        const result = numIslands(input);
+
+        expect(result).to.equal(expected);
+    });
+    it('runs as expected with 3 islands', () => {
+        const input = [
+            ['1','1','0','0','0'],
+            ['1','1','0','0','0'],
+            ['0','0','1','0','0'],
+            ['0','0','0','1','1'],
+        ];
+
+        const expected = 3;
+
+        const result = numIslands(input);
+
+        expect(result).to.equal(expected);
+    });
+});
---
+++ src/numberOfIslands/numberOfIslands.ts
@@ -0,0 +1,9 @@
+/**
+ * Number of islands in a grid
+ * @param grid the grid
+ * @returns number of islands
+ * @description runtime O()
+ */
+export function numIslands(grid: string[][]) {
+    return 0;
+}
