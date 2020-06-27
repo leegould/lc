cf68a9d659fe3ff999773604d4b117745075e12d
Lee <gould.lee@googlemail.com>
2020-06-27 12:06:54 +0100

    game of life

--- src/gameOfLife/gameOfLife.ts
+++ src/gameOfLife/gameOfLife.ts
@@ -4,26 +4,28 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 export function gameOfLife(board: number[][]) {
-    const colLen = board[0].length - 1;
-    const rowLen = board.length - 1;
-    const newBoard: number[][] = [];
+    const copy = board.map((arr) => {
+        return arr.slice();
+    });
+    
+    const colLen = copy[0].length - 1;
+    const rowLen = copy.length - 1;
 
     function liveNeighbours(row: number, col: number) {
         return 0 +
-            (row > 0 && col > 0 ? board[row - 1][col - 1] : 0) +
-            (row > 0 ? board[row - 1][col] : 0) +
-            (row > 0 && col < colLen ? board[row - 1][col + 1] : 0) +
-            (col > 0 ? board[row][col - 1] : 0) +
-            (col < colLen ? board[row][col + 1] : 0) +
-            (row < rowLen && col > 0 ? board[row + 1][col - 1] : 0) +
-            (row < rowLen ? board[row + 1][col] : 0) +
-            (row < rowLen && col < colLen ? board[row + 1][col + 1] : 0);
+            (row > 0 && col > 0 ? copy[row - 1][col - 1] : 0) +
+            (row > 0 ? copy[row - 1][col] : 0) +
+            (row > 0 && col < colLen ? copy[row - 1][col + 1] : 0) +
+            (col > 0 ? copy[row][col - 1] : 0) +
+            (col < colLen ? copy[row][col + 1] : 0) +
+            (row < rowLen && col > 0 ? copy[row + 1][col - 1] : 0) +
+            (row < rowLen ? copy[row + 1][col] : 0) +
+            (row < rowLen && col < colLen ? copy[row + 1][col + 1] : 0);
     }
 
     function getNewValue(row: number, col: number) {
-        const live = board[row][col] === 1;
+        const live = copy[row][col] === 1;
         const nbs = liveNeighbours(row, col);
-        // console.log('cell', row, col, live, nbs);
         if (!live && nbs === 3 || live && nbs > 1 && nbs < 4) {
             return 1;
         } else {
@@ -31,13 +33,11 @@ export function gameOfLife(board: number[][]) {
         }
     }
 
-    for(let i = 0;i< board.length;i++) {
-        const newRow: number[] = [];
-        for(let j = 0; j < board[i].length;j++) {
-            newRow.push(getNewValue(i,j));
+    for(let i = 0;i< copy.length;i++) {
+        for(let j = 0; j < copy[i].length;j++) {
+            board[i][j] = getNewValue(i,j);
         }
-        newBoard.push(newRow);
     }
 
-    return newBoard;
+    return board; // Return for test
 }
