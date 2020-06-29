dd0b9799acee664f6c1eaf906bddabb20489d08f
Lee <gould.lee@googlemail.com>
2020-06-29 10:17:37 +0100

    search first and last position

--- src/firstAndLastPosition/firstAndLastPosition.ts
+++ src/firstAndLastPosition/firstAndLastPosition.ts
@@ -6,5 +6,35 @@
  * @description runtime O()
  */
 export function searchRange(nums: number[], target: number) {
-    return [-1,-1];
+    const indexes = [-1,-1];
+
+    let i = 0;
+    let j = nums.length - 1;
+
+    while(i < j) {
+        const mid = Math.floor((i + j) / 2);
+        if (nums[mid] < target) {
+            i = mid + 1;
+        } else {
+            j = mid;
+        }
+    }
+
+    if (nums[i] !== target) {
+        return indexes;
+    }
+    indexes[0] = i;
+
+    j = nums.length - 1;
+    while(i < j) {
+        const mid = Math.floor(((i + j) / 2) + 1);
+        if (nums[mid] > target) {
+            j = mid - 1;
+        } else {
+            i = mid;
+        }
+    }
+    indexes[1] = j;
+
+    return indexes;
 }
