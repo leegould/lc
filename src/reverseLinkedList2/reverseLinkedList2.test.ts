fbb50bf3c099058156a81290d7dd1f5c73e181bf
Lee <gould.lee@googlemail.com>
2020-07-01 10:52:11 +0100

    reverse linked list

--- src/reverseLinkedList2/reverseLinkedList2.test.ts
+++ src/reverseLinkedList2/reverseLinkedList2.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import { arrayToLinkedList, compareTwoListsByVal, ListNode } from '../util/LinkedList';
+import { arrayToLinkedList, compareTwoListsByVal, ListNode, linkedListToArray } from '../util/LinkedList';
+import { reverseBetween } from './reverseLinkedList2';
 
 describe('reverseLinkedList2', () => {
     it('runs as expected', () => {
@@ -7,8 +8,9 @@ describe('reverseLinkedList2', () => {
         const start = 2;
         const end = 4;
         const expected = arrayToLinkedList([1,4,3,2,5]) as ListNode;
-        const result = reverseBetween(ll, start, end);
+        const result = reverseBetween(ll, start, end) as ListNode;
 
+        console.log('result', linkedListToArray(result));
         
         expect(compareTwoListsByVal(result, expected)).to.be.true;
     });
--- src/reverseLinkedList2/reverseLinkedList2.ts
+++ src/reverseLinkedList2/reverseLinkedList2.ts
@@ -1,19 +1,30 @@
 import { ListNode } from "../util/LinkedList";
 
 export function reverseBetween(head: ListNode | null, start: number, end: number) {
-    if (head) {
-        let node: ListNode | null = head;
-        let counter = 1;
-        while(node && counter < start) {
-            node = node.next;
-            counter++;
-        }
+    let holder = new ListNode(0);
+    holder.next = head;
+    let pre: ListNode | null = holder;
 
-        if (node) {
-            for(let i = 0;i < start - end;i++) {
-                
-            }
+    for(let i = 0;i < start - 1;i++) {
+        if (pre) {
+            pre = pre.next;
         }
+    }
+
+    let startnode = pre?.next || null;
+    let post = startnode?.next || null;
+
+    for(let i = 0;i < start - end;i++) {
+        if (startnode) {
+            startnode.next = post?.next || null;
+        }
+        if (post) {
+            post.next = pre?.next || null;
+        }
+        if (pre) {
+            pre.next = post;
+        }
+        post = startnode?.next || null;
     }
 
     return head;
