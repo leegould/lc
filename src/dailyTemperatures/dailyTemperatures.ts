c9160bbef06b10ef8ec09c6e859d7464ccc66f24
Lee Gould <gould.lee@googlemail.com>
2020-07-21 15:03:46 +0100

    faster daily temps

--- src/dailyTemperatures/dailyTemperatures.ts
+++ src/dailyTemperatures/dailyTemperatures.ts
@@ -2,23 +2,20 @@
  * Daily Temperatures
  * @param T the list of temps
  * @returns the number of days till its warmer
- * @description runtime O(n ^ 2) space O(n)
+ * @description runtime O(n) space O(n)
  */
 export function dailyTemperatures(T: number[]): number[] {
-    const results = [];
+    const res = Array(T.length).fill(0);
+    const stack: any[] = [];
 
-    for(let i = 0; i < T.length;i++) {
+    for(let i = T.length - 1;i >= 0;i--) {
         const temp = T[i];
-        let days = 0;
-        for(let j = 0;j < T.length - i;j++) {
-            const compare = T[i + j];
-            if (compare > temp) {
-                days = j;
-                break;
-            }
+        while(stack.length > 0 && stack[stack.length - 1].temp <= temp) {
+            stack.pop();
         }
-        results.push(days);
+        res[i] = stack.length === 0 ? 0 : stack[stack.length - 1].idx - i;
+        stack.push({temp, idx: i});
     }
-
-    return results;
+    
+    return res;
 };
