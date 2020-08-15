85bf4bdf51aaee245087e4bc7fbe4726f509d71d
Lee Gould <gould.lee@googlemail.com>
2020-08-15 12:33:28 +0100

    much cleaner keyboard rows

--- src/keyboardRow/keyboardRow.ts
+++ src/keyboardRow/keyboardRow.ts
@@ -2,27 +2,16 @@
  * Keyboard row
  * @param words the list of words
  * @returns the words typeable for each row
- * @description runtime O(n * wordlen) space O(n)
+ * @description runtime O(n * len(n)) space O(n)
  */
 export function findWords(words: string[]): string[] {
     const rows = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
 
-    function inCharset(word: string, charset: string) {
-        for(const char of word.split('')) {
-            if (charset.indexOf(char.toLowerCase()) < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    const results = [];
-    for(const word of words) {
-        console.log('check', word, inCharset(word, rows[0]), inCharset(word, rows[0]), inCharset(word, rows[0]))
-        if (inCharset(word, rows[0]) || inCharset(word, rows[1]) || inCharset(word, rows[2])) {
-            results.push(word);
-        }
-    }
-
-    return results;
+    return words.filter(word =>
+        rows.some(row => 
+            [...word.toLowerCase()].every(char =>
+                row.includes(char)
+            )
+        )
+    );
 };
