78e81569d9507935be548e8b23857478e04da9f2
Lee Gould <gould.lee@googlemail.com>
2020-09-05 14:23:03 +0100

    removing need for stack

--- src/minAddToMakeParenValid/minAddToMakeParenValid.ts
+++ src/minAddToMakeParenValid/minAddToMakeParenValid.ts
@@ -2,23 +2,23 @@
  * Minimum Add to Make Parentheses Valid
  * @param S the string
  * @returns how many parens need to be added to make valid
- * @description runtime O(n) space O(n)
+ * @description runtime O(n) space O(1)
  */
 export function minAddToMakeValid(S: string): number {
-    let counter = 0;
-    const stack = [];
+    let open = 0;
+    let closed = 0;
 
     for(let i = 0; i < S.length;i++) {
         if (S[i] === '('){
-            stack.push(S[i]);
+            open++;
         } else if (S[i] === ')') {
-            if (stack.length > 0) {
-                stack.pop();
+            if (open > 0) {
+                open--;
             } else {
-                counter++;
+                closed++;
             }
         }
     }
 
-    return stack.length + counter;
+    return open + closed;
 };
