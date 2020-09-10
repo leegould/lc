2db6776830216d0afd07dcf1ca0f4eb74d922040
Lee Gould <gould.lee@googlemail.com>
2020-09-10 09:43:21 +0100

    slightly improved smaller number than

--- src/smallerNumbersThanCurrent/smallerNumbersThanCurrent.ts
+++ src/smallerNumbersThanCurrent/smallerNumbersThanCurrent.ts
@@ -2,20 +2,9 @@
  * Smaller numbers than current
  * @param nums the list of numbers
  * @returns array of results
- * @description runtime O(n * n) space O(n)
+ * @description runtime O(n lg n) space O(n)
  */
 export function smallerNumbersThanCurrent(nums: number[]): number[] {
-    const results = [];
-
-    for(const num of nums) {
-        let count = 0;
-        for (let i = 0; i < nums.length;i++) {
-            if (nums[i] < num) {
-                count++;
-            }
-        }
-        results.push(count);
-    }
-
-    return results;
+    const sorted = [...nums].sort((a, b) => a - b);
+    return nums.map((num) => sorted.indexOf(num));
 };
